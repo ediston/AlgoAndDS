088b06fc59bd061e3907a85cd2c717100ec021a0
ediston <vivek4dhiman@gmail.com>
2015-03-12 16:20:11 +0100

    Update SparseTable.cpp

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -18,14 +18,21 @@ void process2( )
           M[i][0] = i;
   //compute values from smaller to bigger intervals
       for (j = 1; 1 << j <= N; j++) // till 2^j 
-          for (i = 0; i + (1 << j) - 1 < N; i++)  // till N -(2^j -1)
+          for (i = 0; i + (1 << j) - 1 < N; i++)  // till N -(2^j -1) 
+              // from i till i+2^j-1
               if (A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]])
                   M[i][j] = M[i][j - 1];
               else
                   M[i][j] = M[i + (1 << (j - 1))][j - 1];
 }
 int query(int i, int j) {
+    // we find k such that
+    // j = (i + 2^k -1) + 1 + (2^k -1)
+    // k = Log2(j-i+1)
+    // floor(Log2(j-i+1))
     int k = (int)floor(log((double)j-i+1) / log(2.0));    // 2^k <= (j-i+1)
+    // dividing by log(2.0) necause log here is Log10
+    // Log2x = Log10x/Log2;
     if (A[M[i][k]] <= A[M[j-(1<<k)+1][k]])
         return M[i][k];
     else
