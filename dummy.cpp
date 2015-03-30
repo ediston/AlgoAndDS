fb982c839b1d509ea6f34ae4c847b59bfaf435ed
ediston <vivek4dhiman@gmail.com>
2015-03-30 17:30:10 +0200

    Create dummy.cpp

---
+++ dummy.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define PROG ""
+#define INFILE PROG "in"
+#define OUTFILE PROG "out"
+#define fp(i,f,t) for(int i=f; i<t; i++)
+#define fm(i,f,t) for(int i=f; i>t; i--)
+#define MP(x,y) make_pair(x,y)
+
+using namespace std;
+
+
+int main() {
+    return 0;
+}
