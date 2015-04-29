08bd4baee141b08d8953eb5a2a7dfc2cb8bf0b47
ediston <vivek4dhiman@gmail.com>
2015-04-29 15:34:56 +0200

    Create gcjdummy.cpp

---
+++ gcjdummy.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define PB(x) push_back(x)
+#define fp(i,f,t) for(int i=f; i<t; i++)
+#define fm(i,f,t) for(int i=f; i>t; i--)
+#define MP(x,y) make_pair(x,y)
+
+using namespace std;
+
+
+int main() {
+    int t=0;
+    cin >> t;
+    for(int _case=1; _case<=t; _case++){
+        cout << "Case #"<< _case <<": " ;
+    
+    }
+    return 0;
+}
